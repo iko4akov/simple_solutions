e4c8b6e43c226a62b15b30b16ecbe57415dc273e
iko4ak <iko4akov@gmail.com>
2023-12-17 15:29:55 +0300

    add cerate product and price

--- app/item/service.py
+++ app/item/service.py
@@ -1,22 +1,62 @@
+import json
+
 import stripe
 
-from config.settings import YOUR_DOMAIN
+from config.settings import STRIPE_SECRETE_KEY, YOUR_DOMAIN
 
+from item.models import Item
 
-def create_checkout_session():
+stripe.api_key = STRIPE_SECRETE_KEY
+
+def create_checkout_session(item: Item):
+    product = create_product(item)
+    price = create_price(item).get('data')[0]
+
     try:
         checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': '{{PRICE_ID}}',
-                    'quantity': 1,
-                },
-            ],
-            mode='payment',
-            success_url=YOUR_DOMAIN + '/success',
-            cancel_url=YOUR_DOMAIN + '/cancel',
+          success_url="https://example.com/success",
+          line_items=[{"price": price.get('id'), "quantity": 1}],
+          mode="payment",
         )
+
+        return checkout_session
 
     except Exception as e:
         return str(e)
+
+
+def create_product(item: Item) -> dict:
+    product = None
+
+    try:
+        product = stripe.Product.retrieve(id=str(item.pk))
+
+    except stripe.error.InvalidRequestError as e:
+        if e.http_status == 404:
+            product = stripe.Product.create(id=str(item.pk), name=item.name, description=item.description)
+
+        else:
+            product = {'exception': str(e)}
+
+    finally:
+        return product
+
+def create_price(item: Item) -> dict:
+    price = None
+
+    try:
+        if len(stripe.Price.list(product=item.pk)) > 0:
+            price = stripe.Price.list(product=item.pk)
+
+        else:
+            price = stripe.Price.create(
+                currency="usd",
+                unit_amount_decimal=item.price,
+                product_data={"id": item.pk, "name": item.name},
+            )
+
+    except stripe.error.InvalidRequestError as e:
+        price = {'exception': str(e)}
+
+    finally:
+        return price
--- app/item/views.py
+++ app/item/views.py
@@ -1,14 +1,33 @@
-import stripe
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView, get_object_or_404
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from config.settings import STRIPE_SECRETE_KEY
 
 from item.serializers import ItemSerializer
 
 from item.models import Item
 
-stripe.api_key = STRIPE_SECRETE_KEY
+from item.service import create_checkout_session, create_product
 
-class GetSessionAPIView(RetrieveAPIView):
-    serializer_class = ItemSerializer
-    queryset = Item.objects.all()
+
+class GetSessionAPIView(APIView):
+
+    def get(self, request, pk):
+        item = Item.objects.get(pk=pk)
+        product = create_checkout_session(item)
+
+        return Response({
+            'item': item.__str__(),
+            # 'url': price,
+            'prod': product
+        })
+
+
+class ItemDetailAPIView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'item_page.html'
+
+    def get(self, request, pk):
+        item = Item.objects.get(pk=pk)
+        return Response({'item': item})
